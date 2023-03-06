c0a9dc4681e1df6b0a545a001dd2a206960345db
michael-leonov <michael.leonov96@mail.ru>
2023-03-06 17:38:39 +0300

    init create app(react+ts) + add tailwind + eslint config + prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div className="text-3xl font-bold underline flex justify-center items-center w-screen h-screen">
+      App
+    </div>
+  );
+}
+
+export default App;
