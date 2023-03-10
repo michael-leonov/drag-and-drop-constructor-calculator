999827f80f45ad8ca29e04d347fef9ea0c9276db
michael-leonov <michael.leonov96@mail.ru>
2023-03-10 16:47:17 +0300

    add display component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Display from './components/display'
 
 function App(): JSX.Element {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <div className="w-[539px] flex gap-x-14">
-        <div>Сайдбар</div>
+      <div className="flex gap-x-14">
+        <div className="gap-y-3">
+          <Display />
+        </div>
         <div className="">
           <div className="">Переключатель</div>
           <div>Холст</div>
